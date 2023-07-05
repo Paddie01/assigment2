1eae82699831aa9dfef6e812d01523409f24fa3d
Paddie01 <136599162+Paddie01@users.noreply.github.com>
2023-07-05 08:21:52 -0700

    python
    assignment1

---
+++ assignent3.py
@@ -0,0 +1,8 @@
+number = int(input("enter num: "))
+while number /2 !=0:
+
+    if number %2 == 1:
+       print(number, "is a prime number")
+       break
+else:
+    print (number, "is not a prime number") 
